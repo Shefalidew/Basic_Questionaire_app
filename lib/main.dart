6e7e72b9ad4eeb7f35c4d16bfd95159bf6d3b530
Shefalidew <shefali.dew97@gmail.com>
2022-01-08 13:17:04 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+          backgroundColor: Colors.brown[400],
+          foregroundColor: Colors.white,
+        ),
+        body: Text('This is my default text!'),
+      ),
+    );
+  }
+}
